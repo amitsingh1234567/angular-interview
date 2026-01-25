39eb09ab54c01f343d1af9c3f43612d28a29a723
amit.ranjit.k <amit.ranjit.k>
2026-01-25 22:24:28 +0530

    WIP

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Comp1Component } from './comp1-component/comp1-component';
 import { Comp2Component } from './comp2-component/comp2-component';
 import { Comp3Component } from './comp3-component/comp3-component';
+import { FormComponent } from './form-component/form-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Comp1Component, Comp2Component, Comp3Component],
+  imports: [RouterOutlet, Comp1Component, Comp2Component, Comp3Component, FormComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/form-component/form-component.ts
+++ src/app/form-component/form-component.ts
@@ -1,11 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form-component',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form-component.html',
   styleUrl: './form-component.scss',
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+ userForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      // string
+      name: ['', Validators.required],
+
+      // array (simple values)
+      skills: this.fb.array([
+        this.fb.control('')
+      ]),
+
+      // object
+      address: this.fb.group({
+        city: [''],
+        state: [''],
+        zip: ['']
+      }),
+
+      // array of objects
+      contacts: this.fb.array([
+        this.createContact()
+      ])
+    });
+  }
+
+  /** Getter for skills */
+  get skills(): FormArray {
+    return this.userForm.get('skills') as FormArray;
+  }
+
+  /** Getter for contacts */
+  get contacts(): FormArray {
+    return this.userForm.get('contacts') as FormArray;
+  }
+
+  /** Create contact (object inside array) */
+  createContact(): FormGroup {
+    return this.fb.group({
+      type: [''],
+      value: ['']
+    });
+  }
+
+  addSkill(): void {
+    this.skills.push(this.fb.control(''));
+  }
+
+  addContact(): void {
+    this.contacts.push(this.createContact());
+  }
+
+  removeSkill(index: number): void {
+    this.skills.removeAt(index);
+  }
+
+  removeContact(index: number): void {
+    this.contacts.removeAt(index);
+  }
+
+  submit(): void {
+    const formValue = this.userForm.value;
+    
+    // Filter out empty strings from skills array
+    const filteredSkills = formValue.skills.filter((skill: string) => skill.trim() !== '');
+    
+    // Create clean form value with filtered skills
+    const cleanFormValue = {
+      ...formValue,
+      skills: filteredSkills
+    };
+    
+    console.log(cleanFormValue);
+  }
 }
